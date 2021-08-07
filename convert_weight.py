0575c229597fedc8cf6e9033680b1896b25943ce
Joshua Clet <63855213+Xiu-Wawa@users.noreply.github.com>
2021-08-07 17:19:39 +0800

    Create convert_weight.py

---
+++ convert_weight.py
@@ -0,0 +1,5 @@
+#Ask a user to input its weight in (lbs). Then convert it to (kg) and print it.
+
+weight_lbs = input('What is your weight? (in pounds) ')
+weight_kg = float(weight_lbs)/2.205
+print(round(weight_kg))
